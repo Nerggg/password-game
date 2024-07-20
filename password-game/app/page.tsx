4acbb4ad40cfe6059bfefc6093c8932ce4d232d2
nerggg <13522147@std.stei.itb.ac.id>
2024-07-20 20:59:42 +0700

    feat: main page

--- password-game/app/page.tsx
+++ password-game/app/page.tsx
@@ -1,20 +1,33 @@
+'use client';
+
+import { useState } from 'react';
 import Link from 'next/link';
 
 export default function Home() {
+  const [hoverText, setHoverText] = useState('');
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold mb-8">Navigasi dengan Tombol</h1>
+    // <div className="flex flex-col items-center justify-center min-h-screen py-2 h-screen">
+    <div className="flex flex-col items-center justify-center pt-[15%]">
+      <h1 className="text-4xl font-bold mb-8">pASSword game :D</h1>
       <div className="flex flex-col space-y-4">
-        <Link href="/easy" className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 text-center">
-          Tombol A
+        <Link href="/easy" className="px-4 py-2 border text-white rounded-lg hover:bg-blue-700 text-center"
+            onMouseEnter={() => setHoverText('difficulty easy')}
+            onMouseLeave={() => setHoverText('')}>
+            easy
         </Link>
-        <Link href="/medium" className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-700 text-center">
-          Tombol B
+        <Link href="/medium" className="px-4 py-2 border text-white rounded-lg hover:bg-green-700 text-center">
+            Medium
         </Link>
-        <Link href="/hard" className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-700 text-center">
-          Tombol C
+        <Link href="/hard" className="px-4 py-2 border text-white rounded-lg hover:bg-red-700 text-center">
+            harD
         </Link>
       </div>
+      {hoverText && (
+        <div className="pt-6 text-lg text-gray-700">
+          {hoverText}
+        </div>
+      )}
     </div>
   );
 }
